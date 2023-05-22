0f30795032aa1cbd0c5c23f2b4bb6966e6bf3e37
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-05-21 22:10:53 -0700

    Create main.go
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/nextlinux/docker-inspect/cmd"
+)
+
+var (
+	version   = "No version provided"
+	commit    = "No commit provided"
+	buildTime = "No build timestamp provided"
+)
+
+func main() {
+	cmd.SetVersion(&cmd.Version{
+		Version:   version,
+		Commit:    commit,
+		BuildTime: buildTime,
+	})
+
+	cmd.Execute()
+}
