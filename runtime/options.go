bcedaff94e86fc2c3a48a6c33a30690ed3945fde
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-05-21 22:28:07 -0700

    Create options.go
    Signed-off-by: gitworkflows <118260833+gitworkflows@users.noreply.github.com>

---
+++ runtime/options.go
@@ -0,0 +1,16 @@
+package runtime
+
+import (
+	"github.com/spf13/viper"
+	"github.com/nextlinux/docker-inspect/docker-inspect"
+)
+
+type Options struct {
+	Ci           bool
+	Image        string
+	Source       docker-inspect.ImageSource
+	IgnoreErrors bool
+	ExportFile   string
+	CiConfig     *viper.Viper
+	BuildArgs    []string
+}
